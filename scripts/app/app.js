6d97f7387c534292900e7246abe3d2a699ba6a24
Toma Puljak <toma.puljak@hotmail.com>
2017-05-25 22:09:58 +0200

    Added empty students.css

--- scripts/app/app.js
+++ scripts/app/app.js
@@ -23,32 +23,27 @@ app.run(function(localStorageService) {
 app.config(function($stateProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $stateProvider
-        .state("home",
-            {
-                url: "/",
-                controller: "HomeController",
-                template: `
-                    <h1>DOBRO DOŠLI U IMENIK</h1>
-                    <button ng-click="goToListOfStudents()">Pogledaj učenike</button>
-                `
-            })
-        .state("students",
-            {
-                url: "/students",
-                controller: "StudentController",
-                templateUrl: "students.html"
-            });
+    .state("home", {
+        url: "/",
+        controller: "HomeController",
+        template: `
+            <h1>DOBRO DOŠLI U IMENIK</h1>
+            <button ng-click="goToListOfStudents()">Pogledaj učenike</button>
+        `
+    })
+    .state("students", {
+        url: "/students",
+        controller: "StudentController",
+        templateUrl: "students.html"
+    });
 });
 
-app.controller("HomeController",
-    function($scope, $state) {
-        $scope.goToListOfStudents = function() {
-            $state.go("students");
-        }
-    });
+app.controller("HomeController", function($scope, $state) {
+    $scope.goToListOfStudents = function() {
+        $state.go("students");
+    }
+});
 
-app.controller("StudentController",
-    function($scope, $state, localStorageService) {
-        $scope.students = angular.fromJson(localStorageService.get("students"));
-    });
-
+app.controller("StudentController", function($scope, $state, localStorageService) {
+    $scope.students = angular.fromJson(localStorageService.get("students"));
+});
